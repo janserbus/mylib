51d9f27e9aaf1fc82ba6e8be472f9058b89f8b82
FiWe1 <filipweissen@gmail.com>
2024-04-15 16:30:46 +0200

    cpu loop stress fix - key event

--- treerend/treerend.c
+++ treerend/treerend.c
@@ -298,7 +298,9 @@ void TR_Present()
 
                 break;                
             }
-        }        
+        }
+        //a little break for the cpu...
+        SDL_Delay(10);
     }
 
     SDL_DestroyRenderer(renderer);
